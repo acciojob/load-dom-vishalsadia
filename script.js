21e77262a33cb9a1a3f5cd36e2353b0a1f4020de
vishalsadia <125430740+vishalsadia@users.noreply.github.com>
2024-01-23 11:16:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+  document.addEventListener('DOMContentLoaded', function () {
+            // DOM is loaded, add your code here
+            var body = document.body;
+            body.innerHTML = 'DOM load success';
+        });
